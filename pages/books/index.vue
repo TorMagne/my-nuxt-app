7558b5df80483c319fc566cfc0f261988d7d9e26
TorMagne <tormagne-brekken@outlook.com>
2024-10-04 10:31:25 +0200

    Refactor book creation form UI

--- pages/books/index.vue
+++ pages/books/index.vue
@@ -1,19 +1,38 @@
 <template>
   <section class="book-create">
-    <h1>Create a New Book</h1>
+    <h1 class="font-bold my-4">Create a New Book</h1>
     <form @submit.prevent="submitForm" enctype="multipart/form-data">
-      <div>
-        <label for="name">Book Name:</label>
-        <input type="text" id="name" v-model="form.name" required />
-      </div>
+      <label class="form-control w-full max-w-xs mb-4">
+        <div class="label">
+          <span class="label-text">Book Name</span>
+        </div>
+        <input
+          placeholder="Book name"
+          class="input input-bordered w-full max-w-xs"
+          type="text"
+          id="name"
+          v-model="form.name"
+          required
+        />
+      </label>
 
-      <div>
-        <label for="description">Description:</label>
-        <textarea id="description" v-model="form.description" required></textarea>
-      </div>
+      <label class="form-control mb-4">
+        <div class="label">
+          <span class="label-text">Book description</span>
+        </div>
+        <textarea
+          class="textarea textarea-bordered h-24"
+          placeholder="Book description"
+          id="description"
+          v-model="form.description"
+          required
+        ></textarea>
+      </label>
 
-      <div>
-        <label for="image">Book Image:</label>
+      <label class="form-control w-full max-w-xs mb-4">
+        <div class="label">
+          <span class="label-text">Book Image</span>
+        </div>
         <input
           type="file"
           id="image"
@@ -21,19 +40,12 @@
           @change="handleFileUpload"
           accept="image/*"
           required
+          class="file-input file-input-bordered w-full max-w-xs"
         />
-      </div>
+      </label>
 
-      <button type="submit">Create Book</button>
+      <button type="submit" class="btn btn-success">Create Book</button>
     </form>
-
-    <!-- Success and error messages -->
-    <div v-if="successMessage" class="success-message">
-      {{ successMessage }}
-    </div>
-    <div v-if="errorMessage" class="error-message">
-      {{ errorMessage }}
-    </div>
   </section>
 </template>
 
@@ -53,8 +65,8 @@ const handleFileUpload = (event) => {
 
 const submitForm = async () => {
   // Reset messages
-  successMessage.value = '';
-  errorMessage.value = '';
+  // successMessage.value = '';
+  // errorMessage.value = '';
 
   // Validate form inputs
   if (!form.value.name || !form.value.description || !selectedFile.value) {
@@ -74,7 +86,13 @@ const submitForm = async () => {
     });
 
     // Since $fetch does not throw an error, the request was successful
-    successMessage.value = result.message;
+    // successMessage.value = result.message;
+
+    useToastify(result.message, {
+      type: 'success',
+      autoClose: 3000,
+      position: ToastifyOption.POSITION.TOP_RIGHT,
+    });
     // Optionally, redirect to another page or reset the form
     // router.push('/books'); // Redirect to books list
     form.value.name = '';
@@ -84,34 +102,17 @@ const submitForm = async () => {
     document.getElementById('image').value = '';
   } catch (error) {
     // Handle errors thrown by $fetch
-    errorMessage.value = error?.data?.statusMessage || 'An error occurred.';
+    useToastify('An error occurred', {
+      type: 'error',
+      autoClose: 3000,
+      position: ToastifyOption.POSITION.TOP_RIGHT,
+    });
     console.error(error);
   }
 };
 </script>
 
 <style scoped>
-.book-create {
-  max-width: 600px;
-  margin: 0 auto;
-}
-
-.book-create form div {
-  margin-bottom: 15px;
-}
-
-.book-create label {
-  display: block;
-  margin-bottom: 5px;
-}
-
-.book-create input[type='text'],
-.book-create textarea {
-  width: 100%;
-  padding: 8px;
-  box-sizing: border-box;
-}
-
 .success-message {
   color: green;
   margin-top: 20px;
