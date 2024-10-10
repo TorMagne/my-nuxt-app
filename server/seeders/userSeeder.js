2e34bfb88eda18244b8a8a8afcbc53eca023b167
TorMagne <tormagne-brekken@outlook.com>
2024-10-10 13:27:03 +0200

    Refactor userSeeder.js: Update MongoDB connection string

--- server/seeders/userSeeder.js
+++ server/seeders/userSeeder.js
@@ -1,12 +1,12 @@
 import mongoose from 'mongoose';
 import User from '../models/userModel.js'; // Adjust the path as necessary
-import dotenv from 'dotenv';
+// import dotenv from 'dotenv';
 
-dotenv.config();
+// dotenv.config();
 
 const seedUsers = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {});
+    await mongoose.connect('mongodb://localhost:27017/mydatabase');
 
     // Clear existing users
     await User.deleteMany({});
