eb30e0f1abd863a83bdf815632564d4f370d030a
TorMagne <tormagne-brekken@outlook.com>
2024-10-11 21:31:08 +0200

    Refactor task creation functionality and error handling

--- layouts/default.vue
+++ layouts/default.vue
@@ -119,7 +119,7 @@
             >
           </li>
           <li v-if="AuthStore.user.role === 'admin'">
-            <NuxtLink to="/taskType" activeClass="active-link">
+            <NuxtLink to="/taskTypes" activeClass="active-link">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
@@ -136,6 +136,26 @@
               </svg>
 
               Task types</NuxtLink
+            >
+          </li>
+          <li v-if="AuthStore.user.role === 'admin'">
+            <NuxtLink to="/tasks" activeClass="active-link">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="size-6"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M6 6.878V6a2.25 2.25 0 0 1 2.25-2.25h7.5A2.25 2.25 0 0 1 18 6v.878m-12 0c.235-.083.487-.128.75-.128h10.5c.263 0 .515.045.75.128m-12 0A2.25 2.25 0 0 0 4.5 9v.878m13.5-3A2.25 2.25 0 0 1 19.5 9v.878m0 0a2.246 2.246 0 0 0-.75-.128H5.25c-.263 0-.515.045-.75.128m15 0A2.25 2.25 0 0 1 21 12v6a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 18v-6c0-.98.626-1.813 1.5-2.122"
+                />
+              </svg>
+
+              Tasks</NuxtLink
             >
           </li>
           <button class="btn btn-secondary mt-4" @click="AuthStore.logout">Logout</button>
